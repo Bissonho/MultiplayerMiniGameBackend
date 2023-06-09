4e476ec905aab8a090d33a2077b999159136e748
pbissonhoe <paulovictorbissonho@gmail.comm>
2023-06-09 10:20:52 -0300

    config game

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { SessionService } from './service/player/session.service';
 import { SessionController } from './controller/player/session.controller';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb+srv://pbissonho:paulo1234@cluster0.f0puqtd.mongodb.net/",  {
+  imports: [MongooseModule.forRoot("mongodb+srv://pbissonho:paulo1234@cluster0.f0puqtd.mongodb.net/?retryWrites=true&w=majority?directConnection=true",  {
   }, ),
   MongooseModule.forFeature([{ name: 'Session', schema: SessionSchema }])],
   
